fb77f4181d9351b8b420cbe534a35233a9d3aab2
Leon Engle <leon.engle@example.com>
2025-09-01 08:02:49 -0400

    Add power supply slider

--- Fusor_Workspace/fusor_main.py
+++ Fusor_Workspace/fusor_main.py
@@ -1,11 +1,24 @@
+import tkinter as tk
+
+import power_control as PC
 import vacuum_control as VC
 import communication as com
 
-
 RPi_3b = com.SSHClient("RPi_1_IP", 22, "username", "password", "command_to_send {}")
 turboPump = VC.TurboPump("name")
-supply30kV = VC.PowerSupply("30kV Supply")
+supply27kV = PC.PowerSupply("Fusor Supply", maxVoltage=28000, maxCurrent=0.040)
 
-supply30kV.set_voltage(RPi_3b, 30000)  # Set voltage to 30kV
+root = tk.Tk()
+root.title("Fusor Control Panel")
+root.geometry("400x300")
+root.default_font = ("Times New Roman", 8)
 
-turboPump.setPower(85)
+powerControlLabel = tk.Label(root, text="Desired Fusor Supply Voltage", font=root.default_font)
+powerControlLabel.grid(row=0, column=0, padx=10, pady=10)
+
+powerControlScale = tk.Scale(root, from_=0, to=supply27kV.maxVoltage, orient="horizontal")
+powerControlScale.grid(row=1,column=0, padx=10, pady=10)
+
+powerControlButton = tk.Button(root, text="Set", command=lambda: supply27kV.set_voltage(voltageSetting=int(powerControlScale.get())))
+powerControlButton.grid(row=1,column=1, padx=10, pady=10)
+root.mainloop()
--- Fusor_Workspace/power_control.py
+++ Fusor_Workspace/power_control.py
@@ -1,14 +1,32 @@
 import communication as com
+import random #remove later
 
 class PowerSupply:
-    def __init__(self, name):
+    def __init__(self, name, maxVoltage, maxCurrent):
         self.name = name
-        self.maxCurrent = 0
-        self.maxVoltage = 0
+        self.maxVoltage = maxVoltage
+        self.maxCurrent = maxCurrent
         self.i = 0
         self.v = 0
         self.vDesired = 0
         self.iDesired = 0
     
-    def set_voltage(RPi, voltageSetting):
-        RPi.send_ssh_command(voltageSetting)
+    def set_voltage(self, voltageSetting):
+        self.vDesired = voltageSetting
+        #RPi.send_ssh_command(voltageSetting)
+        print(f"Setting voltage to {voltageSetting}V")
+
+    def set_current(self, RPi, currentSetting):
+        self.iDesired = currentSetting
+        #RPi.send_ssh_command(currentSetting)
+        print(f"Setting current to {currentSetting}A")
+
+    def get_voltage(self, RPi):
+        self.v = random.random() #RPi.send_ssh_command(voltageSetting)
+        print(f"Voltmeter reading is {self.i}V")
+
+    def get_current(self, RPi):
+        self.i = random.random() #RPi.send_ssh_command(voltageSetting)
+        print(f"Current meter reading is {self.i}A")
+
+    #configure interrupt for if max current is exceeded, decrease voltage setting
